65dc9dca17061ce37ec49b6d3c48d1efcf2a920a
smirnov_aa <smirnov_aa@vector.ru>
2025-12-05 01:09:30 +0300

    chore: 05.12.25 01:09

---
+++ src/main/kotlin/snippets/C2_Item14/7.kt
@@ -0,0 +1,10 @@
+package f_C2_Item14.s_7
+
+@DslMarker
+annotation class HtmlDsl
+
+fun table(f: TableDsl.() -> Unit) { /*...*/
+}
+
+@HtmlDsl
+class TableDsl { /*...*/ }
---
+++ src/main/kotlin/snippets/C6_Item36/5.kt
@@ -0,0 +1,11 @@
+package f_C6_Item36.s_5
+
+abstract class Dog {
+    open fun bark() {
+        /*...*/
+    }
+
+    open fun sniff() {
+        /*...*/
+    }
+}
