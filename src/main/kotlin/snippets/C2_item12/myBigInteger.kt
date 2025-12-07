64b8b4cef6ecc7590f17cf51fbf0f1f1d3bb8a9e
smirnov_aa <smirnov_aa@vector.ru>
2025-12-07 14:26:25 +0300

    chore: 07.12.25 14:26

---
+++ src/main/kotlin/snippets/C2_item12/myBigInteger.kt
@@ -0,0 +1,16 @@
+package C1_Item02.myBigInteger
+
+import java.math.BigDecimal
+
+fun main(){
+    val num1 = BigDecimal("1.0")
+    val num2 = BigDecimal("1.00")
+    println(num1 == num2) // false
+    println(num1 >= num2 && num1 <= num2) // true
+    println(num1.compareTo(num2) == 0) // true
+ }
+/*
+false
+true
+true
+ */
